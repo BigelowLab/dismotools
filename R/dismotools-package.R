1c0bdba74ac1fcd7a2525b9255175b4673c2f28a
Ben Tupper <btupper@bigelow.org>
2019-02-04 10:22:28 -0500

    add gbm

--- R/dismotools-package.R
+++ R/dismotools-package.R
@@ -1,9 +1,10 @@
 #' dismotools: Handy tools for the raster and dismo packages.
 #'
-#' Extra functionality for working with raster and dismo packages including model 
-#' reading, model parameter extraction, sampling and extraction from multilayer 
+#' Extra functionality for working with raster and dismo packages including model
+#' reading, model parameter extraction, sampling and extraction from multilayer
 #' raster objects.
-#' 
+#'
 #' @docType package
 #' @name dismotools
+#' @importFrom magrittr %>%
 NULL
