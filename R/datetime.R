202be26e4cc67038e450741cb8fd48e46068387a
Ben Tupper <btupper@bigelow.org>
2020-09-04 11:32:18 -0400

    undo all that S3 stuff for datetime

--- R/datetime.R
+++ R/datetime.R
@@ -5,40 +5,14 @@
 #' @param lut charcater vector of length 12.  x is converted to month number
 #'  and is then used as a index into this look up table
 #' @return charcater vector of season names, same length as x
-date_to_seasonname <- function(x) UseMethod("date_to_seasonname")
-#' @describeIn date_to_seasonname non POSIXct or Date classes
-date_to_seasonname.default <- function(x,
+date_to_seasonname <- function(x,
   lut = c(
     'DJF', 'DJF',
     'MAM', 'MAM', 'MAM',
     'JJA', 'JJA', 'JJA',
     'SON', 'SON', 'SON',
     'DJF')){
-  if (missing(x)) {
-    x <- Sys.Date()
-    return(date_to_seasonname.Date(x))
-  } else {
-    stop("no functionality available for class: ", paste(class(x), collapse = " "))
-  }
-}
-#' @describeIn date_to_seasonname for POSIXct classes
-date_to_seasonname.POSIXct <- function(x,
-  lut = c(
-      'DJF', 'DJF',
-      'MAM', 'MAM', 'MAM',
-      'JJA', 'JJA', 'JJA',
-      'SON', 'SON', 'SON',
-                    'DJF')){
-  lut[lubridate::month(x)]
-}
-#' @describeIn date_to_seasonname for Date classes
-date_to_seasonname.Date <- function(x,
-  lut = c(
-    'DJF', 'DJF',
-    'MAM', 'MAM', 'MAM',
-    'JJA', 'JJA', 'JJA',
-    'SON', 'SON', 'SON',
-    'DJF')){
+  if (missing(x))  x <- Sys.Date()
   lut[lubridate::month(x)]
 }
 
@@ -47,24 +21,8 @@ date_to_seasonname.Date <- function(x,
 #' @export
 #' @param x POSIXct or Date vector
 #' @return POSIXct or Date that starts the season a date belongs to
-date_to_seasonstart <- function(x) UseMethod("date_to_seasonstart")
-#' @describeIn date_to_seasonstart for missing input
-date_to_seasonstart.default <- function(x){
-  if (missing(x)) {
-    x <- Sys.Date()
-    return(date_to_seasonstart.Date(x))
-  } else {
-    stop("no functionality available for class: ", paste(class(x), collapse = " "))
-  }
-}
-#' @describeIn date_to_seasonstart for POSIXct input
-date_to_seasonstart.POSIXct <- function(x){
-    r <- range(x)
-    s <- seq_season(from = r[1], to = r[2])
-    s[findInterval(x, s)]
-}
-#' @describeIn date_to_seasonstart for Date input
-date_to_seasonstart.Date <- function(x){
+date_to_seasonstart <- function(x){
+  if (missing(x)) x <- Sys.Date()
   r <- range(x)
   s <- seq_season(from = r[1], to = r[2])
   s[findInterval(x, s)]
@@ -76,23 +34,8 @@ date_to_seasonstart.Date <- function(x){
 #' @param x POSIXct or Date vector
 #' @param week_length numeric, the number of days per week
 #' @return numeric 1-46 or 1-52 week number
-date_to_week <- function(x) UseMethod("date_to_week")
-#' @describeIn date_to_week for missing input
-date_to_week.default <- function(x, week_length = c(8,7)[1]){
-  if (missing(x)) {
-    x <- Sys.Date()
-    return(date_to_week.Date(x, week_length = week_length))
-  } else {
-    stop("no functionality available for class: ", paste(class(x), collapse = " "))
-  }
-}
-#' @describeIn date_to_week for POSIXct input
-date_to_week.POSIXct <- function(x = Sys.time(), week_length = c(8,7)[1]){
-    J <- as.numeric(format(x, "%j"))
-    (J-1) %/% week_length + 1
-}
-#' @describeIn date_to_week for Date input
-date_to_week.Date <- function(x = Sys.Date(), week_length = c(8,7)[1]){
+date_to_week <- function(x, week_length = c(8,7)[1]){
+  if (missing(x)) x <- Sys.Date()
   J <- as.numeric(format(x, "%j"))
   (J-1) %/% week_length + 1
 }
@@ -128,21 +71,16 @@ as_POSIXct <- function(x, time = '00:00:00', tz = 'UTC', format = '%Y-%m-%d %H:%
 #'      \item{JJA prior summer start dates}
 #'      \item{SON prior autumn start dates}
 #' }
-date_to_seasonprior <- function(x) UseMethod("date_to_seasonprior")
-#' @describeIn date_to_seasonprior for missing input
-date_to_seasonprior.default <- function(x){
-  if (missing(x)) {
-    return(date_to_seasonprior.Date(x = Sys.Date()))
-  } else {
-    stop("no functionality available for class: ", paste(class(x), collapse = " "))
-  }
-}
-#' @describeIn date_to_seasonprior for POSIXct input
-date_to_seasonprior.POSIXct <- function(x = Sys.time()){
+date_to_seasonprior <- function(x){
+  if (missing(x)) x <- Sys.Date()
 
     # get the range of dates, and dial the first one back on year
     r <- range(x)
-    r[1] <- r[1] - 366*24*60*60
+    if (inherits(x, "POSIXt")){
+      r[1] <- r[1] - 366*24*60*60
+    } else {
+      r[1] <- r[1] - 366
+    }
     # get the season starts that cover the same range
     S <- seq_season(from = r[1], to = r[2])
     S_name <- date_to_seasonname(S)
@@ -166,35 +104,6 @@ date_to_seasonprior.POSIXct <- function(x = Sys.time()){
     X <- tibble::as.tibble(X)
     tibble::add_column(X, x, .before = 1)
 }
-#' @describeIn date_to_seasonprior for Date input
-date_to_seasonprior.Date <- function(x = Sys.Date()){
-
-  # get the range of dates, and dial the first one back on year
-  r <- range(x)
-  r[1] <- r[1] - 366
-  # get the season starts that cover the same range
-  S <- seq_season(from = r[1], to = r[2])
-  S_name <- date_to_seasonname(S)
-  fS_name <- as.factor(S_name)
-  SS <- split(S,fS_name)
-
-  x_czn <- date_to_seasonstart(x)
-
-  czn <- c("DJF", "MAM", "JJA", "SON")
-  names(czn) <- czn
-
-  X <- lapply(czn,
-              function(cname){
-                ix <- findInterval(x, SS[[cname]])
-                y <- SS[[cname]][ix]
-                iy <- y == x_czn
-                if (any(iy)) y[iy] <- SS[[cname]][ix[iy]-1]
-                y
-              })
-
-  X <- tibble::as.tibble(X)
-  tibble::add_column(X, x, .before = 1)
-}
 
 #' Generate a sequence of POSIXct or Date by season start dates
 #'
@@ -202,69 +111,59 @@ date_to_seasonprior.Date <- function(x = Sys.Date()){
 #' @param from POSIXct or Date, the starting date
 #' @param to POSIXct or Date, the ending date
 #' @return POSIXct or Date sequence
-seq_season <- function(from, to) UseMethod("seq_season")
-#' @describeIn seq_season for POSIXct input
-seq_season.POSIXct <- function(
-    from = as.POSIXct("2003-12-01 00:00:00", tz = "UTC"),
-    to = Sys.time()){
+seq_season <- function(
+    from = as.Date("2003-01-01"),
+    to = Sys.Date()){
 
-    to = as_POSIXct(to)
+   if (inherits(from, "POSIXt")){
+     to = as_POSIXct(to)
 
-    fy <- lubridate::year(from[1])
-    fm <- lubridate::month(from[1])
-    # note there are 12 entries
-    from <- switch(fm[1],
-        lubridate::ymd_hms(paste0(fy-1,"-12-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy-1,"-12-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-03-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-03-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-03-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-06-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-06-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-06-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-09-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-09-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-09-01 00:00:00"), tz = "UTC", quiet = TRUE),
-        lubridate::ymd_hms(paste0(fy,"-12-01 00:00:00"), tz = "UTC", quiet = TRUE))
+     fy <- lubridate::year(from[1])
+     fm <- lubridate::month(from[1])
+     # note there are 12 entries
+     from <- switch(fm[1],
+         lubridate::ymd_hms(paste0(fy-1,"-12-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy-1,"-12-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-03-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-03-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-03-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-06-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-06-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-06-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-09-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-09-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-09-01 00:00:00"), tz = "UTC", quiet = TRUE),
+         lubridate::ymd_hms(paste0(fy,"-12-01 00:00:00"), tz = "UTC", quiet = TRUE))
+   } else {
+     to <- as.Date(to)
+     fy <- lubridate::year(from[1])
+     fm <- lubridate::month(from[1])
+     # note there are 12 entries
+     from <- switch(fm[1],
+                    lubridate::ymd(paste0(fy-1,"-12-01"),quiet = TRUE),
+                    lubridate::ymd(paste0(fy-1,"-12-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-03-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-03-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-03-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-06-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-06-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-06-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-09-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-09-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-09-01"), quiet = TRUE),
+                    lubridate::ymd(paste0(fy,"-12-01"), quiet = TRUE))
+   }
 
-    seq(from = from[1], to = to[1], by = '3 months')
-}
-#' @describeIn seq_season
-#'  for Date input
-seq_season.Date <- function(
-  from = as.Date("2003-12-01"),
-  to = Sys.Date()){
-  to <- as.Date(to)
-  fy <- lubridate::year(from[1])
-  fm <- lubridate::month(from[1])
-  # note there are 12 entries
-  from <- switch(fm[1],
-                 lubridate::ymd(paste0(fy-1,"-12-01"),quiet = TRUE),
-                 lubridate::ymd(paste0(fy-1,"-12-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-03-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-03-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-03-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-06-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-06-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-06-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-09-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-09-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-09-01"), quiet = TRUE),
-                 lubridate::ymd(paste0(fy,"-12-01"), quiet = TRUE))
   seq(from = from[1], to = to[1], by = '3 months')
 }
-
-
 
 #' Compute today's date as POSIXct
 #'
 #' @export
-#' @param x POSIXct or charcater date to be converted with as.POSIXct()
+#' @param x Date or POSIXct or charcater date to be converted with as_POSIXct()
 #' @param form the form of the date returned, possible a formatting pattern
-#' @return POSIXct
-today <- function(x) UseMethod("today")
-#' @describeIn today for missing input
-today.default <- function(x,
+#' @return POSIXct or Date
+today <- function(x,
   form = c('POSIXct', "Date", "%Y-%m-%d", 'week', 'season')[2]){
   if (missing(x)) x <- Sys.Date()
   switch(form[1],
@@ -274,46 +173,22 @@ today.default <- function(x,
          'POSIXct' = as_POSIXct(x),
          format(x, form[1]))
 }
-#' @describeIn today for POSIXct input
-today.POSIXct <- function(x = as.POSIXct(paste(Sys.Date(), "00:00:00"),tz = 'UTC'),
-    form = c('POSIXct', "Date", "%Y-%m-%d", 'week', 'season')[1]){
-    switch(form[1],
-        'week' = date_to_week(x),
-        'season' = date_to_seasonstart(x),
-        "Date"   = as.Date(x),
-        'POSIXct' = x,
-        format(x, form[1]))
-}
-#' @describeIn today for Date input
-today.Date <- function(x = Sys.Date(),
-  form = c('POSIXct', "Date", "%Y-%m-%d", 'week', 'season')[1]){
-  switch(form[1],
-         'week' = date_to_week(x),
-         'season' = date_to_seasonstart(x),
-         "Date"   = x,
-         'POSIXct' = as_POSIXct(x),
-         format(x, form[1]))
-}
+
 #' Compute the next n day
 #'
 #' @export
 #' @param x Date or POSIXct, starting date, generally \code{today()}
 #' @param n the number of days ahead (or behind if negative)
 #' @return POSIXct or Date
-tomorrow <- function(x) UseMethod("tomorrow")
-#' @describeIn tomorrow for missing input
-tomorrow.default <- function(x, n = 1){
+tomorrow <- function(x, n = 1){
   if (missing(x)) x <- Sys.Date()
-  x + n
+  if (inherits(x, "Date")){
+    x + n
+  } else {
+    x + (n * 86400)
+  }
 }
-#' @describeIn tomorrow for POSIXct input
-tomorrow.POSIXct <- function(x = Sys.time(), n = 1){
-   x + (n * 86400)
-}
-#' @describeIn tomorrow for Date input
-tomorrow.Date <- function(x = Sys.Date(), n = 1){
-  x + n
-}
+
 
 #' Compute the prior n day.
 #'
@@ -323,20 +198,15 @@ tomorrow.Date <- function(x = Sys.Date(), n = 1){
 #' @param x POSIXct date as a starting date, generally \code{today()}
 #' @param n the number of days prior (or ahead if positive)
 #' @return POSIXct
-yesterday <- function(x) UseMethod("yesterday")
-#' @describeIn yesterday for missing input
-yesterday.default <- function(x, n = -1){
+yesterday <- function(x, n = -1){
   if (missing(x)) x <- Sys.Date()
-  x + n
-}
-#' @describeIn yesterday for POSIXct input
-yesterday.POSIXct <- function(x = Sys.time(), n = -1){
+  if (inherits(x, "Date")){
+    x + n
+  } else {
     x + (n * 86400)
+  }
 }
-#' @describeIn yesterday for Date input
-yesterday.Date <- function(x = Sys.Date(), n = -1){
-  x + n
-}
+
 
 #' Compute the doy window given the day of year and window size
 #'
